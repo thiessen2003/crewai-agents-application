b7a735bd42bf0d64cc665cf84070824ca1f8e824
Gabriel Thiessen <thiessengabriel@gmail.com>
2025-01-06 18:29:49 -0500

    running flask

--- be/api.py
+++ be/api.py
@@ -1,3 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
+
+@app.route('/api/crew', methods=['POST'])
+def run_crew(): 
+    return jsonify({"status": "success"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3001)
