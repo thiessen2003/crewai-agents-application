50dfbe024ab3289331c31a5ef77bd7870069b65e
Gabriel Thiessen <thiessengabriel@gmail.com>
2025-01-06 18:42:32 -0500

    setting up additional functions

--- be/api.py
+++ be/api.py
@@ -1,10 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+import uuid
 
 app = Flask(__name__)
 
 @app.route('/api/crew', methods=['POST'])
 def run_crew(): 
+    data = request.json
+    if not data or 'companies' not in data or 'positions' not in data: 
+        abort(400, description="Invalid request with missing data")
+   
+    job_id = str(uuid())
+
     return jsonify({"status": "success"}), 200
+
+@app.route('/api/crew/<job_id>', methods=['GET'])
+def get_status(job_id):
+    return jsonify({"status": f"Getting status for {job_id}"}), 200
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=3001)
