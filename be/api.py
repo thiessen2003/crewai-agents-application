e68ce1f587fa02d9b8c778330362a3030f95a929
Gabriel Thiessen <thiessengabriel@gmail.com>
2025-01-07 09:20:00 -0500

    adding kickoffcrew function

--- be/api.py
+++ be/api.py
@@ -1,7 +1,19 @@
 from flask import Flask, jsonify, request, abort
-import uuid
+from uuid import uuid4
+from threading import Thread
 
 app = Flask(__name__)
+
+def kickoff_crew(job_id: str, companies: list[str], positions: list[str]):
+    print(f"Running crew for {job_id} with companies {companies} and positions {positions}")
+
+    #SETUP THE CREW HERE
+    
+
+
+    #RUN THE CREW HERE
+
+    #LET APP KNOW WE ARE DONE
 
 @app.route('/api/crew', methods=['POST'])
 def run_crew(): 
@@ -9,9 +21,16 @@ def run_crew():
     if not data or 'companies' not in data or 'positions' not in data: 
         abort(400, description="Invalid request with missing data")
    
-    job_id = str(uuid())
+    job_id = str(uuid4())
+    companies = data['companies']
+    positions = data['positions']
 
-    return jsonify({"status": "success"}), 200
+    #Run the crew 
+    thread = Thread(target=kickoff_crew, args=(job_id, companies, positions))
+
+    thread.start()
+
+    return jsonify({"job_id": job_id}), 200
 
 @app.route('/api/crew/<job_id>', methods=['GET'])
 def get_status(job_id):
