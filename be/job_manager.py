4e971aa7a37aa83b3f56bb7ac6c049250c8ca294
Gabriel Thiessen <thiessengabriel@gmail.com>
2025-01-08 10:03:56 -0500

    setting up local db

--- be/job_manager.py
+++ be/job_manager.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass 
 from typing import List, Dict 
 from datetime import datetime
+from threading import Lock
 
 @dataclass
 class Event: 
@@ -13,5 +14,23 @@ class Job:
     events: List[Event]
     result: str
 
+jobs_lock = Lock()
+jobs: Dict[str, "Job"] = {}
+
 def append_event(job_id: str, event_data: str):
-    
+    with jobs_lock: 
+        if job_id in jobs: 
+            print(f"Start job: {job_id}")
+            jobs[job_id] = Job(
+                status="Started",
+                events=[],
+                results=""
+            )
+        else: 
+            print("appending event for job")
+        jobs[job_id].events.append(
+            Event(
+                timestamp=datetime.now(),
+                data=event_data
+            )
+        )
